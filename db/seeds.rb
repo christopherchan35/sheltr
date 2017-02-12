1db4276a982e839dfc079531fbd5ebabb63110d5
Christopher Chan <christopherchan35@gmail.com>
2017-02-11 16:24:01 -0800

    working seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelter.destroy_all
+
+Shelter.create(
+  name: "St Anthony Foundation",
+  location: "150 Golden Gate Ave",
+  total_beds: 50,
+  open_beds: 20)
+
+Shelter.create(
+  name: "One Brick",
+  location: "237 Kearny St",
+  total_beds: 30,
+  open_beds: 10)
+
+Shelter.create(
+  name: "St Vincent de Paul Society",
+  location: "525 5th St",
+  total_beds: 40,
+  open_beds: 15)
+
+Shelter.create(
+  name: "Episcopal Community Services",
+  location: "165 8th St",
+  total_beds: 20,
+  open_beds: 10)
+
+Shelter.create(
+  name: "Tenderloin Housing Clinic",
+  location: "126 Hyde St",
+  total_beds: 35,
+  open_beds: 5)
+
+Shelter.create(
+  name: "Larkin Street Services",
+  location: "701 Sutter St",
+  total_beds: 15,
+  open_beds: 3)
