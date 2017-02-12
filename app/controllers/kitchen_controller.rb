827bc0dd618afdddca6c5cd9421887af17772c9e
Christopher Chan <christopherchan35@gmail.com>
2017-02-11 23:46:37 -0800

    kitchen views is now working

--- app/controllers/kitchen_controller.rb
+++ app/controllers/kitchen_controller.rb
@@ -1,2 +1,19 @@
 class KitchenController < ApplicationController
+  def index
+    @kitchens = Kitchen.all
+  end
+
+  def googleKey
+    if request.xhr?
+      render :json => ENV['GOOGLE_MAPS']
+    end
+  end
+
+  def gmarkers2
+    @gmarkers = Kitchen.all
+    if request.xhr?
+      render :json => @gmarkers
+    end
+  end
+
 end
